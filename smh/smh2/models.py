65df3b21e625dc437b65bd5fd8fb61f903eff898
Parker Williams <pwilliams@lingotek.com>
2016-11-03 23:13:43 -0600

    Update to master. Ability to get sentiment analysis now, Update modles.py file to include tweet storage. Messed withtemplate for index. Need to get streaming stock data next.

--- smh/smh2/models.py
+++ smh/smh2/models.py
@@ -27,3 +27,23 @@ class Stock(models.Model):
     def __str__(self):
         stock = name + ": " + ticker
         return self.stock
+
+class Tweet(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    lang = models.CharField(max_length=10)
+    location = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    screen_name = models.CharField(max_length=100)
+    text = models.CharField(max_length=1000)
+    text_polarity = models.DecimalField(max_digits=10, decimal_places=10)
+    text_subjectivity = models.DecimalField(max_digits=10, decimal_places=10)
+    time_zone = models.CharField(max_length=100)
+    user_followers = models.IntegerField
+    user_is_following = models.IntegerField
+    user_tweets = models.IntegerField
+    user_total_likes = models.IntegerField
+    verified = models.CharField(max_length=100)
+
+    def __str__(self):
+        basic_info = 'Screen Name: ' + screen_name + ', Location: ' + location + ', Tweet: ' + text + ', Tweet Polarity: ' + text_polarity + ', Tweet Subjectivity: ' + text_subjectivity
+        return self.basic_info
--- smh/smh2/views.py
+++ smh/smh2/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
 from TwitterAPI import TwitterAPI
-from .models import User, Stock
+from .models import User, Stock, Tweet
+from textblob import TextBlob
+
 import pyrebase
 import json
 import argparse
@@ -25,26 +27,15 @@ def get_service():
     scoped_credentials.authorize(http)
     return discovery.build('language', 'v1beta1', http=http)
 
-def analyze_sentiment(text):
-    body = {
-        'document': {
-            'type': 'PLAIN_TEXT',
-            'content': text,
-        }
-    }
-
-    service = get_service()
-
-    request = service.documents().analyzeSentiment(body=body)
-    response = request.execute()
-
-    return response
-
 def index(request):
     #try:
     #except .DoesNotExist:
         #raise Http404("Page does not exist")
     return render(request, "smh2/index.html")
+
+def textblobSentimentAnalysis(text):
+    sentiment = TextBlob(text)
+    return sentiment
 
 def startTrack(request):
 
@@ -58,8 +49,8 @@ def startTrack(request):
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()
 
-    #TRACK_TERMS = ['walmart', 'apple', 'google', 'gopro']
-    TRACK_TERMS = ['Walmart','Exxon Mobil','Apple','Berkshire Hathaway','McKesson','UnitedHealth Group','CVS Health','General Motors','Ford Motor','AT&T','General Electric','AmerisourceBergen','Verizon','Chevron','Costco','Fannie Mae','Kroger','Amazon.com','Amazon','Walgreens Boots Alliance','Walgreens','HP','Cardinal Health','Express Scripts Holding','J.P. Morgan Chase','Boeing','Microsoft','Bank of America Corp.','Wells Fargo','Home Depot','Citigroup','Phillips 66','IBM','Valero Energy','Anthem','Procter & Gamble','Alphabet','Comcast','Target','Johnson & Johnson','MetLife','Archer Daniels Midland','Marathon Petroleum','Freddie Mac','PepsiCo','United Technologies','Aetna',"Lowe’s",'UPS','AIG','Prudential Financial','Intel','Humana','Disney','Cisco Systems','Pfizer','Dow Chemical','Sysco','FedEx','Caterpillar','Lockheed Martin']
+    TRACK_TERMS = ['walmart', 'apple', 'google', 'gopro']
+    #TRACK_TERMS = ['Walmart','Exxon Mobil','Apple','Berkshire Hathaway','McKesson','UnitedHealth Group','CVS Health','General Motors','Ford Motor','AT&T','General Electric','AmerisourceBergen','Verizon','Chevron','Costco','Fannie Mae','Kroger','Amazon.com','Amazon','Walgreens Boots Alliance','Walgreens','HP','Cardinal Health','Express Scripts Holding','J.P. Morgan Chase','Boeing','Microsoft','Bank of America Corp.','Wells Fargo','Home Depot','Citigroup','Phillips 66','IBM','Valero Energy','Anthem','Procter & Gamble','Alphabet','Comcast','Target','Johnson & Johnson','MetLife','Archer Daniels Midland','Marathon Petroleum','Freddie Mac','PepsiCo','United Technologies','Aetna',"Lowe’s",'UPS','AIG','Prudential Financial','Intel','Humana','Disney','Cisco Systems','Pfizer','Dow Chemical','Sysco','FedEx','Caterpillar','Lockheed Martin']
     CONSUMER_KEY = '0zvfK5MNq0dBg4Ymi1aZ3LgeJ'
     CONSUMER_SECRET = 'fXIYNVx5cjKR7dHz8QskYXCBQCNZR07Dsk0qf1TpnqXseVGN9m'
     ACCESS_TOKEN_KEY = '885756391-gy5jzaAGuIkSlVEMo38UTPAFIkYtUxPPDV6U5Xlt'
@@ -70,51 +61,36 @@ def startTrack(request):
              ACCESS_TOKEN_KEY,
              ACCESS_TOKEN_SECRET)
 
-    # walmart_pattern = re.compile('(@?[Ww][Aa][Ll][Mm][Aa][Rr][Tt])')
-    # apple_pattern = re.compile('(@?[Aa][Pp][Pp][Ll][Ee])')
-    # google_pattern = re.compile('(@?[Gg][Oo][Oo][Gg][Ll][Ee])')
-
     counter = db.child('counter').get().val()
     if counter is None:
         counter = 0
     r = api.request('statuses/filter', {'track': TRACK_TERMS})
     for item in r.get_iterator():
-        try:
-            google_sentiment = analyze_sentiment(item['text'])
-        except:
-            google_sentiment = ""
         data = {
-            #'user_description': item['user']['description'],
-            #'user_location': item['user']['location'],
-            'text': item['text'],
-            'coords': item['coordinates'],
-            'geo': item['geo'],
+            'lang': item['user']['lang'],
+            'location': item['user']['location'],
+            'name': item['user']['name'],
             'screen_name': item['user']['screen_name'],
-            'user_created': item['user']['created_at'],
-            'followers': item['user']['followers_count'],
-            'id_str': item['id_str'],
-            'tweet_created_at': item['created_at'],
-            'retweets': item['retweet_count'],
-            'bg_color': item['user']['profile_background_color'],
-            'google_sentiment': google_sentiment
+            'text': item['text'],
+            'time_zone': item['user']['time_zone'],
+            'user_followers': item['user']['followers_count'],
+            'user_is_following': item['user']['friends_count'],
+            'user_tweets': item['user']['statuses_count'],
+            'user_total_likes': item['user']['favourites_count'],
+            'verified': item['user']['verified'],
         }
-        # line = item['text']
-        # if walmart_pattern.match(line):
-        #     walmart_magnitude = walmart_magnitude = walmart_magnitude + google_sentiment['documentSentiment']['magnitude']
-        #     walmart_polarity = walmart_polarity = walmart_polarity + google_sentiment['documentSentiment']['polarity']
-        #     walmart_count = walmart_count + 1
-        # elif apple_pattern.match(line):
-        #     apple_magnitude = apple_magnitude + google_sentiment['documentSentiment']['magnitude']
-        #     apple_polarity = apple_polarity + google_sentiment['documentSentiment']['polarity']
-        #     apple_count = apple_count + 1
-        # elif google_pattern.match(line):
-        #     google_magnitude = google_magnitude + ['documentSentiment']['magnitude']
-        #     google_polarity = google_polarity + google_sentiment['documentSentiment']['polarity']
-        #     google_count = google_count + 1
-        db.child('tweets').push(data)
-        counter = counter + 1
-        db.child('counter').set(counter)
-        #print(counter)
+        #The polarity score is a float within the range [-1.0, 1.0].
+        #The subjectivity is a float within the range [0.0, 1.0]
+            #0.0 is very objective
+            #1.0 is very subjective
+        textblob_sentiment = textblobSentimentAnalysis(data['text'])
+        data['text_polarity'] = textblob_sentiment.sentiment.polarity
+        data['text_subjectivity'] = textblob_sentiment.sentiment.subjectivity
+
+        if data['lang'] == 'en':
+            db.child('tweets').push(data)
+            counter = counter + 1
+            db.child('counter').set(counter)
 
 def terms_conditions(request):
     return HttpResponse("Terms and conditions page.")
