a954524087c0efc4e3ce3f929dca619d9fd03cfd
Gregory Bird <gregbird12@gmail.com>
2016-12-07 23:42:47 -0700

    got the views.py file working with the tweet cleaning and sentimet analysis

--- smh/analysis/views.py
+++ smh/analysis/views.py
@@ -4,17 +4,76 @@ from django import forms
 
 import re
 from textblob import TextBlob
+from nltk.corpus import stopwords
+
 from .textstat import textstatistics
+from .apostrophe import APPOSTROPHES
+from .slang import SLANGS
+
 # Create your views here.
 def tweet(request):
+	cached_stop_words = stopwords.words("english")
 	form = TweetForm();
 
 	if request.method == 'POST':
 		form = TweetForm(request.POST)
 		if form.is_valid():
 			tweet = form.cleaned_data.get('tweet')
+			## Start Text Cleaning
+			text = tweet
+			text = text.replace(',', '')
+			text = text.replace('"', '')
+			text = text.replace("''", '')
+			if text.startswith('"') and text.endswith('"'):
+				text = text[1:-1]
+
+			# Apostrophe Lookup
+			new_text_1 = []
+			for word in text.split():
+				word_lower = word.lower()
+				if word_lower in APPOSTROPHES:
+					new_text_1.append(APPOSTROPHES[word_lower])
+				else:
+					new_text_1.append(word)
+			text_1 = " ".join(new_text_1)
+
+			# Caching Stop-Words Part II
+			new_text_2 = []
+			for word in text_1.split():
+				if word not in cached_stop_words:
+					new_text_2.append(word)
+			text_2 = " ".join(new_text_2)
+
+			# Remove URLs
+			new_text_3 = []
+			for word in text_2.split():
+				if not word.startswith("http") or word.startswith("www") or word.startswith("(h"):
+					new_text_3.append(word)
+			text_3 = " ".join(new_text_3)
+
+			# Remove RT @name
+			new_text_4 = []
+			for word in text_3.split():
+				word = re.sub('RT', '', word)
+				word = re.sub('@\w+:', '', word)
+				word = re.sub('@\w+', '', word)
+				new_text_4.append(word)
+			text_4 = " ".join(new_text_4)
+
+			# Slangs Lookup
+			new_text_5 = []
+			for word in text_4.split():
+				word_lower = word.lower()
+				if word_lower in SLANGS:
+					new_text_5.append(SLANGS[word_lower])
+				else:
+					new_text_5.append(word)
+			text_5 = " ".join(new_text_5)
+			cleaned_tweet = text_5
+
+
     	## Start TextBlob Sentiment Analysis
-			textblob_hold_sentiment = TextBlob(tweet)
+			textblob_hold_sentiment = TextBlob(text_5)
             # Polarity = how negative(-1.0), neutral(0), positive(1.0)
 			textblob_senitment_polarity = textblob_hold_sentiment.sentiment.polarity
 			# Subjectivity = very objective(0.0), very subjective(1.0)
@@ -22,8 +81,8 @@ def tweet(request):
         ## End TextBlob Sentiment Analysis
 
 	        ## Start Readability Analytics
-			if re.search('[a-zA-Z]', tweet):
-				txtstats = textstat.textstatistics()
+			if re.search('[a-zA-Z]', text_5):
+				txtstats = textstatistics()
                 # Returns grade level using Lisear Write Formula
 				linsear_write = txtstats.linsear_write_formula(tweet)
 				# Returns grade level based on 3,000 most commonly used english words
@@ -55,8 +114,22 @@ def tweet(request):
                 #     flesch_reading_ease = None
                 #     standard = None
 	        ## End Readability Analytics
+			print('Tweet:', tweet)
+			print('Cleaned Tweet: ', cleaned_tweet)
+			print('textblob_hold_sentiment:', textblob_hold_sentiment)
+			print('textblob_senitment_subjectivity:', textblob_senitment_subjectivity)
+			print('linsear_write:', linsear_write)
+			print('dale_chall:', dale_chall)
+			print('smog:', smog)
+			print('Fog:', fog)
+			print('ari:', ari)
+			print('flesch_kincaid:', flesch_kincaid)
+			print('coleman_liau_index', coleman_liau_index)
+			print('standard:', standard)
 
-			return HttpResponse('Some test values' + textblob_hold_sentiment)
+			print('EVERYTHING SEEMED TO HAVE WORKED')
+
+			return HttpResponse('Some test values')
 
 	template_vars = {
 		'form': form,
